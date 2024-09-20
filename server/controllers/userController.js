64a297977122bf696138048ed72a20691c4ffb6e
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-09-20 09:46:14 -0500

    Subscription Routing Update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -398,6 +398,8 @@ const archiveProposal = async (req, res) => {
   }
 };
 
+
+
 const makeSubscriptionPayment = async (req, res) => {
   const { priceId } = req.body;
 
@@ -412,11 +414,13 @@ const makeSubscriptionPayment = async (req, res) => {
   let customerId = user.stripeCustomerId;
   if (!customerId) {
     const customer = await stripe.customers.create({
-      email: user.email, // Ensure you include the user's email
+      email: user.email,
     });
     customerId = customer.id;
     user.stripeCustomerId = customerId;
     await user.save();
+  } else {
+
   }
 
   try {
@@ -425,12 +429,12 @@ const makeSubscriptionPayment = async (req, res) => {
       customer: customerId,
       line_items: [
         {
-          price: priceId, // Use the provided priceId
+          price: 'price_1PdwW3DfXxf0bxwGjLCd8htC',
           quantity: 1,
         },
       ],
       mode: 'subscription',
-      success_url: `${process.env.ORIGIN}/profile?success=true`, // Redirect to profile on success
+      success_url: `${process.env.ORIGIN}?success=true`,
       cancel_url: `${process.env.ORIGIN}?canceled=true`,
     });
 
@@ -441,7 +445,6 @@ const makeSubscriptionPayment = async (req, res) => {
     res.status(500).json({ error: 'Failed to create Stripe session' });
   }
 };
-
 
 const cancelSubscription  = async (req, res) => {
   try {
