523de2bb4b536363bba6dd77c30ec457389e794f
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-10-10 17:17:32 -0500

    create dummy user for user just view/voting

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -9,6 +9,7 @@ const requireAuth = async (req, res, next) => {
 
     if (authorization) {
       const token = authorization.split(' ')[1];
+
       if (token && token !== process.env.DUMMY_TOKEN) {
         const decodedToken = jwt.verify(token, process.env.SECRET);
 
@@ -27,16 +28,22 @@ const requireAuth = async (req, res, next) => {
       }
     }
 
-    req.user = user; // Set user object in request, null if not authenticated
+    // If no token, generate a new dummy token for the request
+    if (!user) {
+      const newDummyToken = jwt.sign({ _id: process.env.DUMMY_USER }, process.env.SECRET, { expiresIn: '1h' });
+      req.user = { _id: process.env.DUMMY_USER };
+      req.headers.authorization = `Bearer ${newDummyToken}`; // Set the new dummy token in headers
+    } else {
+      req.user = user; // Set the authenticated user object
+    }
+
     next();
   } catch (error) {
     console.error('Authorization error:', error.message);
-    next(); // Proceed even if unauthorized, but req.user will be null
+    next(); // Proceed even if unauthorized
   }
 };
 
 module.exports = requireAuth;
-
-
 
 
