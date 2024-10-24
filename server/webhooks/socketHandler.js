6a16741cc9fe678e143f8e11dd77e24a72974f01
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-10-24 13:36:36 -0500

    Re implement socket io testing

--- server/webhooks/socketHandler.js
+++ server/webhooks/socketHandler.js
@@ -1,24 +1,23 @@
 // socketHandlers.js
 module.exports = (io) => {
     io.on('connection', (socket) => {
-      console.log('New client connected');
-  
-      socket.on('joinProposal', (roomId) => {
-        socket.join(roomId);
-        console.log(`User joined room: ${roomId}`);
-      });
-  
-      socket.on('voteSubmitted', (roomId, newVote) => {
-        io.to(roomId).emit('voteSubmitted', {
-          proposalId: roomId,
-          newVote,
+        console.log('New client connected');
+
+        socket.on('joinProposal', (roomId) => {
+            socket.join(roomId);
+            console.log(`User joined room: ${roomId}`);
         });
-      });
-  
-      socket.on('disconnect', () => {
-        console.log('User disconnected');
-      });
+
+        // Update this line to emit 'newVote' instead of 'voteSubmitted'
+        socket.on('voteSubmitted', (roomId, newVote) => {
+            io.to(roomId).emit('newVote', newVote); // Emit 'newVote' event with the newVote data
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected');
+        });
     });
-  };
+};
+
   
   
