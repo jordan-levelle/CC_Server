1a194a7d20b437ae0e5f1f0bc35e466890f57f05
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-10-25 11:33:02 -0500

    voteEmitter.on move outside socket connection

--- server/webhooks/socketHandler.js
+++ server/webhooks/socketHandler.js
@@ -1,4 +1,4 @@
-// In your socketHandlers.js
+// In socketHandlers.js
 module.exports = (io, voteEmitter) => {
     io.on('connection', (socket) => {
         console.log('New client connected');
@@ -8,16 +8,17 @@ module.exports = (io, voteEmitter) => {
             console.log(`User joined room: ${roomId}`);
         });
 
-        // Listen for vote events from the EventEmitter
-        voteEmitter.on('newVote', (roomId, newVote) => {
-            console.log(`Emitting 'newVote' to room ${roomId}`);
-            io.to(roomId).emit('newVote', newVote); // Emit 'newVote' event with the newVote data
-        });
-
         socket.on('disconnect', () => {
             console.log('User disconnected');
         });
     });
+
+    // Register the voteEmitter listener once, outside of the socket connection event
+    voteEmitter.on('newVote', (roomId, newVote) => {
+        console.log(`Emitting 'newVote' to room ${roomId}`);
+        io.to(roomId).emit('newVote', newVote);
+    });
 };
+
 
   
