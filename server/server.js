1d0903c76b4a49a3d14f8fa381b33c675bed918e
jordan-levelle <76881733+jordan-levelle@users.noreply.github.com>
2024-11-14 13:56:11 -0600

    simplified proposal participation into submitvote cntrl func/server cors

--- server/server.js
+++ server/server.js
@@ -15,16 +15,9 @@ const { initGFSBucket } = require('./utils/gridfs.js');
 const app = express();
 const server = createServer(app);
 
-const corsOptions = {
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  credentials: true,
-};
 
 
-app.use(cors(corsOptions)); 
-app.options('*', cors(corsOptions)); // Allow preflight requests for all routes
+app.use(cors()); 
 
 
 // Middleware
